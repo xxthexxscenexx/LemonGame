789f9328565dad1a8884d50b3aae38a4a3da498d
Rosie <Rosie@roses-macbook-pro.local>
2016-01-03 13:47:52 -0500

    Added all of the variables and functions from view controllers into swift code

--- MixedDrinks/ViewController.swift
+++ MixedDrinks/ViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 // Main View Controller
 class ViewController: UIViewController {
     // Variables for the Lemonade Game
-    @IBOutlet weak var moneyMainView: UILabel! // Money listed on main view 
-    
-    
+    @IBOutlet weak var moneyMainView: UILabel! // Money listed on main view
+    // Level
+    // Day 
+    // Time
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // Added Functions for the lemonade game 
-    
-
+    // Added Functions for the lemonade game
 
 }
 
 // Status View Controller
 class Status: UIViewController {
     // Variables for the Lemonade Game
-    
+    @IBOutlet weak var MoneyStatus: UILabel! // Money listed on status view 
+    // Current status for supplies
+    @IBOutlet weak var CurrentLemons: UILabel!
+    @IBOutlet weak var CurrentSugar: UILabel!
+    @IBOutlet weak var CurrentCubes: UILabel!
+    // Purchase supplies 
+    @IBOutlet weak var BuyLemons: UILabel!
+    @IBOutlet weak var BuySugar: UILabel!
+    @IBOutlet weak var BuyCubes: UILabel!
+    // Mix drinks 
+    @IBOutlet weak var NumLemons: UILabel!
+    @IBOutlet weak var NumSugar: UILabel!
+    @IBOutlet weak var NumCubes: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,15 +58,42 @@ class Status: UIViewController {
     }
     
     // Added Functions for the lemonade game
+    // Purchase supplies buttons 
+    @IBAction func incLemons(sender: UIButton) {
+    }
+    @IBAction func decLemons(sender: UIButton) {
+    }
+    @IBAction func incSugar(sender: UIButton) {
+    }
+    @IBAction func decSugar(sender: UIButton) {
+    }
+    @IBAction func incCubes(sender: UIButton) {
+    }
+    @IBAction func decCubes(sender: UIButton) {
+    }
     
-    
+    // Mix drinks buttons
+    @IBAction func addLemons(sender: UIButton) {
+    }
+    @IBAction func subLemons(sender: UIButton) {
+    }
+    @IBAction func addSugar(sender: UIButton) {
+    }
+    @IBAction func subSugar(sender: UIButton) {
+    }
+    @IBAction func addCubes(sender: UIButton) {
+    }
+    @IBAction func subCubes(sender: AnyObject) {
+    }
     
 }
 
 // Game Play View Controller
 class Play: UIViewController {
     // Variables for the Lemonade Game
-    
+    // Customers 
+    // Taste preference 
+    // Lemonade acidity and coldness
     
     override func viewDidLoad() {
         super.viewDidLoad()
