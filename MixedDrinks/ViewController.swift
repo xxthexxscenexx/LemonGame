974834be6f8256aa5417ed958381049cfad901ee
Rosie <Rosie@roses-macbook-pro.local>
2016-01-03 13:36:05 -0500

    View controllers added to swift code by adding two different classes for status and playing the game.

--- MixedDrinks/ViewController.swift
+++ MixedDrinks/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MixedDrinks
+//  MixedDrinks - Lemonade game
 //
 //  Created by Rosie  on 1/3/16.
 //  Copyright © 2016 Rosie . All rights reserved.
@@ -8,8 +8,13 @@
 
 import UIKit
 
+// Main View Controller
 class ViewController: UIViewController {
-
+    // Variables for the Lemonade Game
+    @IBOutlet weak var moneyMainView: UILabel! // Money listed on main view 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Added Functions for the lemonade game 
+    
 
 
 }
+
+// Status View Controller
+class Status: UIViewController {
+    // Variables for the Lemonade Game
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Added Functions for the lemonade game
+    
+    
+    
+}
+
+// Game Play View Controller
+class Play: UIViewController {
+    // Variables for the Lemonade Game
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Added Functions for the lemonade game
+    
+    
+    
+}
+
 
