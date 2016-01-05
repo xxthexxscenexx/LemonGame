1bfde59e94d9ad630bea239989cba9281c36fcae
Rosie <Rosie@roses-macbook-pro.local>
2016-01-04 19:40:49 -0500

    Core Data error solved! The data is now saved and retrieved correctly from the application for lemon/sugar/cube amounts.

--- MixedDrinks/ViewController.swift
+++ MixedDrinks/ViewController.swift
@@ -9,11 +9,6 @@
 import UIKit
 import CoreData
 
-let appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
-let context: NSManagedObjectContext = appDel.managedObjectContext
-
-let newInventory = NSEntityDescription.insertNewObjectForEntityForName("Inventory", inManagedObjectContext: context)
-
 // MAIN CONTROLLER
 class ViewController: UIViewController {
     // Variables for the Lemonade Game
@@ -35,9 +30,9 @@ class ViewController: UIViewController {
 // STATUS CONTROLLER
 class Status: UIViewController {
     // Variables for the Lemonade Game
-    @IBOutlet weak var amntLemons: UILabel!
-    @IBOutlet weak var amntSugar: UILabel!
-    @IBOutlet weak var amntCubes: UILabel!
+    @IBOutlet weak var amntLemon: UITextField!
+    @IBOutlet weak var amntSugar: UITextField!
+    @IBOutlet weak var amntCube: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,19 +47,63 @@ class Status: UIViewController {
     // Added Functions for the lemonade game
     // SAVE amount of inventory at the start of the day 
     @IBAction func startDay(sender: UIButton) {
+        let appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        // References the App Delegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        // References the Context / Information
         
-        newInventory.setValue(100, forKey: "lemons")
-        newInventory.setValue(100, forKey: "cubes")
-        newInventory.setValue(100, forKey: "sugar")
+        let newInventory = NSEntityDescription.insertNewObjectForEntityForName("Inventory", inManagedObjectContext: context)
+        // Add new inventory items to the database with context from Entity Inventory 
+        
+        newInventory.setValue(amntLemon.text, forKey: "lemons")
+        newInventory.setValue(amntCube.text, forKey: "cubes")
+        newInventory.setValue(amntSugar.text, forKey: "sugar")
         
         do {
             try context.save()
         } catch {
-        //error handling 
+            //error handling 
+            print("There was an error saving data.")
         }
         
         print(newInventory)
         print("Object saved")
+        
+            } // end funct
+    
+    // LOAD inventory amounts
+    @IBAction func LoadData(sender: UIButton) {
+        let appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        // References the App Delegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        // References the Context / Information
+
+        // RETRIEVE Data that was saved
+        do {
+            let request = NSFetchRequest(entityName: "Inventory")
+            // ask for a request
+            let results:NSArray = try context.executeFetchRequest(request)
+            // store all information sent back into rsults
+            
+            if (results.count > 0){ // If there is information then print it out
+                for res in results{
+                    let lem = res.valueForKey("lemons")
+                    let sug = res.valueForKey("sugar")
+                    let cub = res.valueForKey("cubes")
+                    print("Lemons " + (lem! as! String)) // Print out information as strings 
+                    print("Sugar " + (sug! as! String))
+                    print("Cubes " + (cub! as! String))
+                    
+                } // end for loop
+            }else{                  // If there is no information inform user
+                print("There are no results to load")
+            } // end if statement
+            
+        } catch {
+            //error handling
+            print("There was an error retrieving data.")
+        } // end do catch
+
     }
     
 }
@@ -88,5 +127,3 @@ class Play: UIViewController {
     
     
 }
-
-
